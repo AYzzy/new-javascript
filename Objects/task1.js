ad97568b780cbd0e9b2e7c65c688b0b819af0140
ayomide <omoniyiayomide987@gmail.com>
2024-09-17 11:57:51 +0100

    commit

--- Objects/task1.js
+++ Objects/task1.js
@@ -4,8 +4,11 @@ const book ={
 	yearPublished: 1925
 }
 
+const{title, author, yearPublished} = book
 let bookTitle = book.title
 let bookAuthor = book.author
 let bookResult = bookTitle + " by " +bookAuthor
+
+console.log(`${title} by ${author}.`)
 
 console.log(bookResult)
