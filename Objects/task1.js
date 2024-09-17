7e05b319d129189f28ca5df1f09a0331ce447b74
ayomide <omoniyiayomide987@gmail.com>
2024-09-17 11:32:14 +0100

    commit

---
+++ Objects/task1.js
@@ -0,0 +1,11 @@
+const book ={
+	title: 'The Great Gatsby',
+	author: 'F. Scott Fitzgerald',
+	yearPublished: 1925
+}
+
+let bookTitle = book.title
+let bookAuthor = book.author
+let bookResult = bookTitle + " by " +bookAuthor
+
+console.log(bookResult)
