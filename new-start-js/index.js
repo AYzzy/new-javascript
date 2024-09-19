bea55ecbc6b16eda4788e730b37b1853dfdd7708
ayomide <omoniyiayomide987@gmail.com>
2024-09-19 16:02:27 +0100

    commiy

--- new-start-js/index.js
+++ new-start-js/index.js
@@ -1,3 +1,4 @@
+// Slice and Splice 
 let array = [1,2,3,"hommie", true];
 
 //console.log(array)
@@ -21,7 +22,36 @@ let arrayOfNumber= [1,2,3,4,5,6,7]
 
 let slice = arrayOfNumber.slice(2,4)
 console.log(slice)
-
-let answer = arrayOfNumber.splice(2,4,30,32) //first it picks the index,the amount of value to be removed, replace 
+let arr = [7,4,5,6,9]
+let answer = arrayOfNumber.splice(1,0,6,9, ...arr) //first it picks the index,the amount of value to be removed, replace 
 console.log(arrayOfNumber)
 console.log(answer)
+
+// ForEach
+let arrs = [3,4,5,6,7,8]
+
+arrs.forEach((element)=>{
+  let answer = element * 2
+  console.log(answer)
+})
+
+// FILTER
+
+function evenNumbers(arr){
+  return arr.filter((number)=> number % 2 ==0)
+}
+
+function getFriutLength(array){
+  return array.filter((fruit)=> fruit.length >5) 
+}
+
+// map
+function multiplyByTwo(array){
+  return array.map((number)=> number * 2)
+}
+
+let getName = (array)=>{
+  return array.map((obj)=>obj.name)
+}
+
+module.exports = {evenNumbers,getFriutLength,multiplyByTwo,getName}
---
+++ new-start-js/index.test.js
@@ -0,0 +1,31 @@
+const { default: expect } = require("expect")
+const {evenNumbers,getFriutLength,multiplyByTwo,getName} = require("./index.js")
+
+test('test for even number',()=>{
+	let arr = [3,4,5,6,7,8]
+	let result = evenNumbers(arr)
+	let expected = [4,6,8]
+	expect(result).toEqual(expected)
+})
+
+test("test that fruit length is greater than five",()=>{
+	let fruit = ["banana","orange","pear"]
+	let result = getFriutLength(fruit)
+	let expected =["banana","orange"]
+	expect(result).toEqual(expected)
+})
+
+test("multiply number by two",()=>{
+  let arr = [3,4,5,6,7,8]
+  let result = multiplyByTwo(arr)
+  let expected =[6,8,10,12,14,16]
+  expect(result).toEqual(expected)
+
+})
+
+test("map through an array of object",()=>{
+  let arr = [{name: "John",age: 25,},{name: "Jane",age: 24}]
+  let result = getName(arr)
+  let expected = ["John", "Jane"]
+  expect(result).toEqual(expected) 
+})
